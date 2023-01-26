ff680bf610cff082ed278ab81f6470a3b87009ff
sgoby <sangzhihong@163.com>
2023-01-26 16:39:29 +0800

    update log level

--- logger.go
+++ logger.go
@@ -1,12 +1,12 @@
 package glog
 
 import (
-	"log"
-	fmt "github.com/sgoby/glog/gfmt"
 	"bufio"
+	fmt "github.com/sgoby/glog/gfmt"
+	"io"
+	"log"
 	"os"
 	"strings"
-	"io"
 )
 
 type logWriter interface {
@@ -25,130 +25,124 @@ type Logger struct {
 	bufWriter   *bufio.Writer
 }
 
-//
-func newLogger(cnf Config) (lg *Logger,err error){
-	if len(cnf.Tag) < 1{
+func newLogger(cnf Config) (lg *Logger, err error) {
+	if len(cnf.Tag) < 1 {
 		cnf.Tag = defautlTag
 	}
-	if len(cnf.Level) < 1{
+	if len(cnf.Level) < 1 {
 		cnf.Level = "Debug"
 	}
 	cnf.Level = strings.TrimSpace(cnf.Level)
 	lv := 0
-	for i,val := range logLevelMap{
-		if val == strings.ToUpper(cnf.Level){
+	for i, val := range logLevelMap {
+		if val == strings.ToUpper(cnf.Level) {
 			lv = int(i)
 			break
 		}
 	}
-	if len(cnf.SplitType) < 1{
+	if len(cnf.SplitType) < 1 {
 		cnf.SplitType = SplitDaily
 	}
 	cnf.limitSize = getByteSizeByM(cnf.SplitType)
 	lg = &Logger{
-		cnf:cnf,
-		calldepth:3,
-		logLevel:uint(lv),
+		cnf:       cnf,
+		calldepth: 3,
+		logLevel:  uint(lv),
 	}
-	lg.mlogger,err = lg.getLogger()
-	return lg,err
+	lg.mlogger, err = lg.getLogger()
+	return lg, err
 }
 
-
-//
-func (l *Logger)Debug(args ...interface{}) {
+func (l *Logger) Debug(args ...interface{}) {
 	if l.logLevel > DEBUG {
 		return
 	}
-	l.output(DEBUG,"",args...)
+	l.output(DEBUG, "", args...)
 }
-func (l *Logger)DebugF(format string, args ...interface{}) {
+func (l *Logger) DebugF(format string, args ...interface{}) {
 	if l.logLevel > DEBUG {
 		return
 	}
-	l.output(DEBUG,format, args...)
+	l.output(DEBUG, format, args...)
 }
 
-//
-func (l *Logger)Info(args ...interface{}) {
+func (l *Logger) Info(args ...interface{}) {
 	if l.logLevel > INFO {
 		return
 	}
-	l.output(INFO,"",args...)
+	l.output(INFO, "", args...)
 }
-func (l *Logger)InfoF(format string, args ...interface{}) {
+func (l *Logger) InfoF(format string, args ...interface{}) {
 	if l.logLevel > INFO {
 		return
 	}
-	l.output(INFO,format, args...)
+	l.output(INFO, format, args...)
 }
 
-//
-func (l *Logger)Warn(args ...interface{}) {
+func (l *Logger) Warn(args ...interface{}) {
 	if l.logLevel > WARN {
 		return
 	}
-	l.output(WARN,"",args...)
+	l.output(WARN, "", args...)
 }
-func (l *Logger)WarnF(format string, args ...interface{}) {
+func (l *Logger) WarnF(format string, args ...interface{}) {
 	if l.logLevel > WARN {
 		return
 	}
-	l.output(WARN,format, args...)
+	l.output(WARN, format, args...)
 }
 
-//
-func (l *Logger)Error(args ...interface{}) {
+func (l *Logger) Error(args ...interface{}) {
 	if l.logLevel > ERROR {
 		return
 	}
-	l.output(ERROR,"",args...)
+	l.output(ERROR, "", args...)
 }
-func (l *Logger)ErrorF(format string, args ...interface{}) {
+func (l *Logger) ErrorF(format string, args ...interface{}) {
 	if l.logLevel > ERROR {
 		return
 	}
-	l.output(ERROR,format, args...)
+	l.output(ERROR, format, args...)
 }
 
-//
-func (l *Logger)Fatal(args ...interface{}) {
+func (l *Logger) Fatal(args ...interface{}) {
 	if l.logLevel > FATAL {
 		return
 	}
-	l.output(FATAL,"",args...)
+	l.output(FATAL, "", args...)
 }
-func (l *Logger)FatalF(format string, args ...interface{}) {
+func (l *Logger) FatalF(format string, args ...interface{}) {
 	if l.logLevel > FATAL {
 		return
 	}
-	l.output(FATAL,format, args...)
+	l.output(FATAL, format, args...)
 }
-//
-func (l *Logger)PanicRuntimeCaller(args ...interface{}){
+
+func (l *Logger) PanicRuntimeCaller(args ...interface{}) {
 	runMsg := readRuntimeCaller()
 	argsStr := fmt.Sprintf("Panic: runtime error. %v", args...)
-	l.output(FATAL,"%s%s", argsStr,runMsg)
+	l.output(FATAL, "%s%s", argsStr, runMsg)
 }
-//======================================
-func (l *Logger)output(lv uint,format string, args ...interface{}) {
+
+// ======================================
+func (l *Logger) output(lv uint, format string, args ...interface{}) {
 	lvTag, ok := logLevelMap[lv]
 	if !ok {
 		return
 	}
 	//
 	var err error
-	if l.mlogger != nil{
+	if l.mlogger != nil {
 		logstr := ""
-		if len(format) > 0{
+		if len(format) > 0 {
 			logstr = fmt.Sprintf(format, args...)
-		}else{
+		} else {
 			logstr = fmt.Sprint(args...)
 		}
 
-		if mlogio,ok := l.mlogger.(*Logio);ok {
+		if mlogio, ok := l.mlogger.(*Logio); ok {
 			err = mlogio.OutputByLv(l.calldepth, lvTag, logstr)
-		}else{
+		} else {
 			err = l.mlogger.Output(l.calldepth, fmt.Sprintf("[%s] %s", lvTag, logstr))
 		}
 	}
@@ -157,27 +151,28 @@ func (l *Logger)output(lv uint,format string, args ...interface{}) {
 	}
 }
 
-//
-func (l *Logger) Write(p []byte) (n int, err error){
-	l.output(INFO,"",string(p))
-	return len(p),nil
+func (l *Logger) Write(p []byte) (n int, err error) {
+	if l.logLevel > INFO {
+		return len(p), nil
+	}
+	l.output(INFO, "", string(p))
+	return len(p), nil
 }
 
-//
-func (l *Logger)getLogger() (g ILogger,err error){
-	if strings.ToLower(strings.TrimSpace(l.cnf.LogType)) == "syslog"{
+func (l *Logger) getLogger() (g ILogger, err error) {
+	if strings.ToLower(strings.TrimSpace(l.cnf.LogType)) == "syslog" {
 		return l.createSyslogLogger()
 	}
 	//
-	lg := NewLogio(Ldate|Ltime|Lshortfile)
-	logfile,err := createLogFile(&l.cnf)
-	if err != nil{
-		return nil,err
+	lg := NewLogio(Ldate | Ltime | Lshortfile)
+	logfile, err := createLogFile(&l.cnf)
+	if err != nil {
+		return nil, err
 	}
 	if l.cnf.AlsoStdout {
 		lg.ResetWriter(io.MultiWriter(logfile, os.Stdout))
-	}else{
+	} else {
 		lg.ResetWriter(logfile)
 	}
-	return lg,nil
+	return lg, nil
 }
